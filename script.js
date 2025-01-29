fa0b110cff15c69ffec908e5867fa6ea15a8e683
Bus Army Dude <busarmydude@gmail.com>
2025-01-29 12:17:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
             method: 'POST',
             headers: {
                 'Accept': 'application/vnd.github.v3+json',
-                'Authorization': `token YOUR_GITHUB_PERSONAL_ACCESS_TOKEN`,
+                'Authorization': `token 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`,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
