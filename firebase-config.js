0660974ff5b3ca2062693b3e85ee70528e7ae187
Bus Army Dude <busarmydude@gmail.com>
2025-05-13 12:33:18 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
-// TODO: Replace with your Firebase project config
+/ Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "XXXXXXX",
-  appId: "1:XXXX:web:XXXXXX"
+  apiKey: "AIzaSyApZG9pI7NwWxdMcflLEqpyDaC5uOhWCWU",
+  authDomain: "rivers-work-tracker.firebaseapp.com",
+  projectId: "rivers-work-tracker",
+  storageBucket: "rivers-work-tracker.firebasestorage.app",
+  messagingSenderId: "962060928306",
+  appId: "1:962060928306:web:480c427466fc501257c32b",
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
