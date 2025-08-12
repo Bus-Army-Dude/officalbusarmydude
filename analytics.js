737291db4f9027d7683dcaa3e6b4d8b168fd1ee1
Caleb Kritzar <ckritzar53@busarmydude.org>
2025-08-12 12:31:33 -0400

    Delete analytics.js

--- analytics.js
+++
@@ -1,7 +0,0 @@
-// Google Analytics (moved to external JS file)
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-E0RSQS1MS7');
